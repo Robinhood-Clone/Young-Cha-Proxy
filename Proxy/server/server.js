c1f0a924d462427002283a5dd3231634453e4860
Young Cha <youngc0905@gmail.com>
2019-12-11 20:51:28 -0800

    proxy with modules loaded in one port

---
+++ Proxy/server/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+const path = require('path');
+
+app.listen(port, () => console.log(`Listening in on port ${port}!`));
+
+app.use('/', express.static(path.join(__dirname, '../public/lib')));
