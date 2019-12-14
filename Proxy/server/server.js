af49468b8d4b0f62dd8241c9cddbee9b763d329a
Young Cha <youngc0905@gmail.com>
2019-12-14 15:56:22 -0800

    implemented styling and links to other modules

--- Proxy/server/server.js
+++ Proxy/server/server.js
@@ -6,3 +6,6 @@ const path = require('path');
 app.listen(port, () => console.log(`Listening in on port ${port}!`));
 
 app.use('/', express.static(path.join(__dirname, '../public/lib')));
+
+app.use('/stocks/:whatever', express.static(path.join(__dirname, '../public/lib')));
+
